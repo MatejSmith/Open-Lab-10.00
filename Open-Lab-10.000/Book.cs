f8e4d56a3eca79ec9625c7f4f20d365bbd209d4e
matej kováč <mtjkovac@gmail.com>
2020-11-19 18:24:10 +0100

    Create and edit class

---
+++ Open-Lab-10.000/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._00
+{
+    class Book
+    {
+        public string title { get; }
+        private int pages { get; }
+
+        public Book (string title)
+        {
+            this.title = title;
+        }
+
+        
+    }
+}
