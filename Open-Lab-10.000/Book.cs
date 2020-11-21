6e0abfc43eaf6d02b77aca0facb69a62d2d9a0f8
matej kováč <mtjkovac@gmail.com>
2020-11-21 19:13:06 +0100

    book with description

--- Open-Lab-10.000/Book.cs
+++ Open-Lab-10.000/Book.cs
@@ -6,12 +6,74 @@ namespace Open_Lab_10._00
 {
     class Book
     {
-        public string title { get; }
-        private int pages { get; }
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
 
-        public Book (string title)
+        public string Title
         {
-            this.title = title;
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                pages = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+
+        public int ReleaseDate
+        {
+            get
+            {
+                return releaseDate;
+            }
+            set
+            {
+                releaseDate = value;
+            }
+        }
+        public void VypisUdaje()
+        {
+            Console.WriteLine("Title: {0}\nPages: {1}\nCategory: {2}\nAuthor: {3}\nRelease date: {4}", Title, Pages, Category, Author, ReleaseDate);
         }
 
         
--- Open-Lab-10.000/Program.cs
+++ Open-Lab-10.000/Program.cs
@@ -6,8 +6,15 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
-            Book LOTR = new Book("Harry Potter");
-            Console.WriteLine(LOTR.title);
+            Book LOTR = new Book();
+
+            LOTR.Title = "Popol všetkých zarovná";
+            LOTR.Pages = 401;
+            LOTR.Category = "Detektívny román";
+            LOTR.Author = "Dominik Dán";
+            LOTR.ReleaseDate = 2005;
+
+            LOTR.VypisUdaje();
             Console.ReadKey();
         }
     }
