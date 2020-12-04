22d4127901a106d63895df4b489bacb0ada0e1fe
MatejSmith <mtjkovac@gmail.com>
2020-12-04 09:51:04 +0100

    edit HOBIT category to categoryList

--- Open-Lab-10.000/Program.cs
+++ Open-Lab-10.000/Program.cs
@@ -9,17 +9,16 @@ namespace Open_Lab_10._00
             Book LOTR = new Book();
             Book LOTR2 = new Book();
             Book LOTR3 = new Book("Detvan", 200);
-            Book HOBIT = new Book("The Hobbit", 310, "Fantasy Roman", "J. R. R. Tolkien", 1937);
+            Book HOBIT = new Book("The Hobbit", 310, Book.categoryList[4], "J. R. R. Tolkien", 1937);
 
             LOTR.Title = "Popol všetkých zarovná";
             LOTR.Pages = 240;
-            LOTR.Category = "Detektívny román";
             LOTR.Author = "Dominik Dán";
             LOTR.ReleaseDate = 2005;
 
             Console.WriteLine("Nazov: " + LOTR.Title);
             Console.WriteLine("Pocet stran: " + LOTR.Pages);
-            Console.WriteLine("Zaner: " + LOTR.Category);
+            Console.WriteLine("Zaner: " + Book.categoryList[5]);
             Console.WriteLine("Autor: " + LOTR.Author);
             Console.WriteLine("Datum vydania: " + LOTR.ReleaseDate + "\n");
 
